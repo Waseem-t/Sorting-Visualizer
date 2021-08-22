68823c4a7a00f0d1a83625aba58a43d72704bf3c
Waseem-t <89344310+Waseem-t@users.noreply.github.com>
2021-08-22 19:29:50 +0530

    Completed My Project

---
+++ Animation.js
@@ -0,0 +1,162 @@
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
+const RED = "#FF4166";
+const BLUE = "#0074D9"
+const NAVY = "#000080";
+const GRAY = "#333333";
+const GREEN = "#2ECC45";
+
+// Initialize the canvas
+const canvas = document.getElementById("canvas");
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+const ctx = canvas.getContext("2d");
+
+// slider for controlling the speed of the animation
+const speedControl = document.getElementById("Speed");
+
+let speed = speedControl.value;
+speedControl.oninput = function() {
+    speed = 120 - speedControl.value;
+}
+
+// variables used to store the state of each animation frame
+let states = [];
+let timer = [];
+
+// slider for controlling the length of the array
+const lengthControl = document.getElementById("Length");
+
+let length = lengthControl.value;
+lengthControl.oninput = function() {
+    length = lengthControl.value;
+}
+
+// create an random array with size of n
+function createArray(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * (CANVAS_HEIGHT - 25)) + 10);
+    }
+    return arr;
+}
+
+// stop the animation at this moment
+function stop() {
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+}
+
+// selection sort animation
+function startSelectionSort() {
+
+    // clear previous animation
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+
+    // clear the states
+    states = [];
+
+    // run the SelectionSort
+    let arr = createArray(length);//size of array
+    selectionSort(arr, states);//it adds the number of states [array, orderIndex, curCompareIndex, curMinIndex]
+
+    // show the animation
+    for (let i = 0; i < states.length; i++) {
+        timer.push(setTimeout(
+            drawSelect, speed * i,
+             states[i][0], 
+            states[i][1],
+             states[i][2], 
+             states[i][3]));//invokes drawselect() method
+    }
+}
+
+
+
+// insertion sort animation
+function startInsertionSort() {
+
+    // clear previous animation
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+
+    // clear the states
+    states = [];
+
+    // run the InsertionSort
+    let arr = createArray(length);
+    insertionSort(arr, states);
+
+    // show the animation
+    for (let i = 0; i < states.length; i++) {
+        timer.push(setTimeout(drawInsert, speed * i, states[i][0], states[i][1], states[i][2]));
+    }
+}
+
+// merge sort animation
+function startMergeSort() {
+
+    // clear previous animation
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+
+    // clear the states
+    states = [];
+
+    // run the MergeSort
+    let arr = createArray(length);
+    mergeSort(arr, states);
+
+    // show the animation
+    for (let i = 0; i < states.length; i++) {
+        timer.push(setTimeout(drawMerge, speed * i, states[i][0], states[i][1], states[i][2], states[i][3]));
+    }
+}
+
+// 2-way quick sort animation
+function startQuickSort2Way() {
+
+    // clear previous animation
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+
+    // clear the states
+    states = [];
+
+    // run the 2-way quick sort
+    let arr = createArray(length);
+    quickSort2Way(arr, states);
+
+    // show the animation
+    for (let i = 0; i < states.length; i++) {
+        timer.push(setTimeout(drawQuick2Way, speed * i, states[i][0], states[i][1], states[i][2],
+            states[i][3], states[i][4], states[i][5], states[i][6]));
+    }
+}
+
+// heap sort animation
+function startHeapSort() {
+
+    // clear previous animation
+    timer.forEach((sto) => {
+        clearTimeout(sto);
+    });
+
+    // clear the states
+    states = [];
+
+    // run heap sort
+    let arr = createArray(length);
+    heapSort(arr, states);
+
+    // show the animation
+    for (let i = 0; i < states.length; i++) {
+        timer.push(setTimeout(drawHeap, speed * i, states[i][0], states[i][1]));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sorting-Visualizer
+• A web application using HTML, CSS, JS to visualize various sorting algorithms. 
+• Implementation of animations, effects, and core functionalities (sorting algorithms) using JavaScript. 
